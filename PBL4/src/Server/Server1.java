03e27f695fce4dca3c2999f8dd27b1b4df3d002d
cuongnguyen99 <kugayuma231@gmail.com>
2020-12-06 16:15:22 +0700

    Da xu ly logic REQ, REL, ACQ

--- PBL4/src/Server/Server1.java
+++ PBL4/src/Server/Server1.java
@@ -1,18 +1,55 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import Interface.Server1_Interface;
 
-public class Server1 implements Server1_Interface{
-	public void printQueue()
+public class Server1 {
+	public static int currentPort = 5001;
+	public static int server2Port = 5002;
+	public static int time_logic = 0;
+//	public static Socket server1Socket;
+//	public void printQueue()
+//	{
+//		
+//	}
+	public static void sendMess(String mess, int time_logic, int port) throws Exception
 	{
-		
+		//Tạo socket cho client kết nối đến server qua ID address và port
+		Socket server1Socket = new Socket("localhost",port);
+		//Tạo output stream nối với Socket
+		DataOutputStream outToServer = new DataOutputStream(server1Socket.getOutputStream());
+		//Gửi thông tin tới Server thông qua output stream đã nối với socket
+		outToServer.writeBytes(mess);
+		server1Socket.close();
 	}
-	public void sendMess(String mess, int time_logic, int source)
+//	public void receiveMess()
+//	{
+//		
+//	}
+	public static void main(String[] args) throws Exception
 	{
-		
-	}
-	public void receiveMess()
-	{
-		
+		ServerSocket welcomSocket = new ServerSocket(currentPort);
+		sendMess("REQ",time_logic, server2Port);
+		//Chờ yêu cầu từ client
+		Socket con = welcomSocket.accept();
+		//Tạo input stream, nối tới socket
+		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		//Tạo output stream, nối tới socket
+		DataOutputStream outToClient = new DataOutputStream(con.getOutputStream());
+		//Đọc thông tin từ socket
+		String from_client = inFromClient.readLine();
+		from_client.trim();
+		if(from_client.equalsIgnoreCase("ACQ"))
+		{
+			System.out.println("ACQ");
+			System.out.println("Da tien vao doan gang, chuan bi giai phong tai nguyen...");
+			sendMess("REL", time_logic, server2Port);
+			System.out.println("Da giai phong tai nguyen!");
+		}
 	}
 }
--- PBL4/src/Server/Server2.java
+++ PBL4/src/Server/Server2.java
@@ -1,16 +1,61 @@
 package Server;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server2 {
-	public void printQueue()
+	public static int currentPort = 5002;
+	public static int server1Port = 5001;
+	public static int time_logic = 0;
+//	public void printQueue()
+//	{
+//		
+//	}
+	public static void sendMess(String mess, int time_logic, int port) throws Exception
+	{
+		//Tạo socket cho client kết nối đến server qua ID address và port
+		Socket server1Socket = new Socket("localhost",port);
+		//Tạo output stream nối với Socket
+		DataOutputStream outToServer = new DataOutputStream(server1Socket.getOutputStream());
+		//Gửi thông tin tới Server thông qua output stream đã nối với socket
+		outToServer.writeBytes(mess);
+		server1Socket.close();
+	}
+	public static void receiveMess()
 	{
 		
 	}
-	public void sendMess(String mess, int time_logic, int source)
+	public static void main(String[] args) throws Exception
 	{
+		String from_client;
+		String to_client;
+		//Tạo socket server, chờ tại cổng 5002
+		ServerSocket welcomSocket = new ServerSocket(currentPort);
 		
-	}
-	public void receiveMess()
-	{
-		
+		while (true)
+		{
+			//Chờ yêu cầu từ client
+			Socket con = welcomSocket.accept();
+			//Tạo input stream, nối tới socket
+			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			//Tạo output stream, nối tới socket
+			DataOutputStream outToClient = new DataOutputStream(con.getOutputStream());
+			//Đọc thông tin từ socket
+			from_client = inFromClient.readLine();
+			from_client.trim();
+//			System.out.println(from_client);
+			if(from_client.equalsIgnoreCase("REQ"))
+			{
+				System.out.println("REQ");
+				sendMess("ACQ", time_logic, server1Port);
+			}
+			else if(from_client.equalsIgnoreCase("REL"))
+			{
+				System.out.println("REL");
+			}
+		}
 	}
 }
